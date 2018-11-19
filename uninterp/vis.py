08819b9b6a3c24993ba98ab31bfdb7a48e497225
Alex <a.schaaf@abdn.ac.uk>
2018-11-19 12:24:52 +0000

    kwargs and defaults

--- uninterp/vis.py
+++ uninterp/vis.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
 
 
-def plot_seis_section(cube, i, direction="inline", extent=None, aspect=None):
+def plot_seis_section(cube, i, direction="inline", extent=None, aspect=None,
+                      plot_kwargs={}, imshow_kwargs={}):
     """Plots seismic section at position i of given cube. Designed especially for use with ipywidgets.
 
     Args:
@@ -15,13 +16,21 @@ def plot_seis_section(cube, i, direction="inline", extent=None, aspect=None):
         matplotlib.axes
     """
     if extent is None:
-        extent = (None, None, None, None, None, None)
+        extent = (0, cube.shape[0], 0, cube.shape[1], 0, cube.shape[2])
 
-    fig, ax = plt.subplots(figsize=(15,10))
+    pkwargs = {"figsize": (15, 10)}
+    pkwargs.update(plot_kwargs)
+    imkwargs = {"interpolation": "bicubic", "cmap": "seismic"}
+    imkwargs.update(imshow_kwargs)
+
+    fig, ax = plt.subplots(**pkwargs)
     if direction == "inline":
-        ax.imshow(cube[i,:,:].T, cmap="seismic", interpolation="bicubic", extent=extent[:2] + extent[4:], aspect=aspect)  # extent=EXTENT[:2] + EXTENT[4:],
+        ax.imshow(cube[i,:,:].T,
+                  extent=extent[:2] + extent[4:], aspect=aspect, **imkwargs)
     elif direction == "xline":
-        ax.imshow(cube[:,i,:].T, cmap="seismic", interpolation="bicubic", extent=extent[2:4] + extent[4:], aspect=aspect)
+        ax.imshow(cube[:,i,:].T,
+                  extent=extent[2:4] + extent[4:], aspect=aspect, **imkwargs)
     elif direction == "timeslice":
-        ax.imshow(cube[:,:,i].T, cmap="seismic", interpolation="bicubic", extent=extent[:4], aspect=aspect)
+        ax.imshow(cube[:,:,i].T,
+                  extent=extent[:4], aspect=aspect, **imkwargs)
     return ax
